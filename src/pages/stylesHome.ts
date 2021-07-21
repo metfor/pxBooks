1c7e47c7e42f993c0afd9f90dfea7b4e5a81ac2a
metfor <mateus_frade@hotmail.com>
2021-07-21 18:47:36 -0300

    Home-Header

---
+++ src/pages/stylesHome.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+export const Container= styled.header`
+    height:58.1rem;
+    background:#FCDB00 ;
+    
+`
+export const Content= styled.div`
+
+    font-family: 'Open Sans', sans-serif;
+    max-width:1120px;
+    margin:0 auto;
+    padding: 1rem 1rem 12rem;
+    
+    img{
+        margin-top: 2rem;
+        margin-left: 2rem;
+    }
+    h1{
+        font-weight: 700;
+        margin-top: 8rem;
+    }
+    h2{
+        font-weight:400 ;
+        font-size:1rem;
+        margin-top:2rem;
+    }
+    .book{
+        margin-left: 45rem;
+        margin-top: -20rem;
+    }
+    `
+export const Container2 = styled.div`
+    height:58.1rem;
+    background:white ;
+`
+export const Footer=styled.footer`
+    height:58.1rem;
+    background:black ;
+
+`
