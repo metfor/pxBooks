a18d33ad7e2479105ce716137ca4c847d846caee
metfor <mateus_frade@hotmail.com>
2021-08-26 13:50:30 -0300

    feat: responsividade Page Books

--- src/pages/books/styles.ts
+++ src/pages/books/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   @media (max-width: 782) {
-    height: 88.1rem;
+    height: 81.1rem;
   }
   @media (max-width: 2520) {
     height: 51.1rem;
@@ -19,11 +19,21 @@ export const Content = styled.div`
 `;
 
 export const BookBox = styled.div`
-  margin: 4.3rem auto;
+@media(min-width:680px){
+ margin: 4.3rem auto;
 
   img {
     margin: 2rem;
   }
+}
+@media(max-width:680px){
+
+  margin:1rem auto;
+
+  img{
+    margin:0.5rem 1.1rem;
+  }
+}
 `;
 export const Title = styled.h1``;
 export const MyImage = styled.img`
